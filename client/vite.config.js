693c522e711a6e95bb64a9ed791cdaf48133f668
praneeth-katuri <praneethkaturi@outlook.com>
2024-08-11 01:20:00 +0530

    Minor configuration updates and cleanup

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // where our Express backend is running
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
